c1425d9b5a7c2bfa835d63a21e9e8aa77d751317
syuzabu <syuzabu@gmail.com>
2016-11-08 11:09:49 +0900

    ソース管理

---
+++ frmMain.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace namapo
+{
+  partial class frmMain
+  {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows フォーム デザイナーで生成されたコード
+
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.cbxNic = new System.Windows.Forms.ComboBox();
+            this.lblNic = new System.Windows.Forms.Label();
+            this.lblIPa = new System.Windows.Forms.Label();
+            this.txtIPaL = new System.Windows.Forms.TextBox();
+            this.lblMac = new System.Windows.Forms.Label();
+            this.txtMacL = new System.Windows.Forms.TextBox();
+            this.lblPayload = new System.Windows.Forms.Label();
+            this.lblInv = new System.Windows.Forms.Label();
+            this.txtInv = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtPortL = new System.Windows.Forms.TextBox();
+            this.lblL = new System.Windows.Forms.Label();
+            this.txtIPaR = new System.Windows.Forms.TextBox();
+            this.lblR = new System.Windows.Forms.Label();
+            this.txtMacR = new System.Windows.Forms.TextBox();
+            this.txtPortR = new System.Windows.Forms.TextBox();
+            this.btnRecv = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtFrasize = new System.Windows.Forms.TextBox();
+            this.lblFrasize = new System.Windows.Forms.Label();
+            this.nudPayload = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPayload)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(497, 190);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(115, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "送信テスト開始";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // cbxNic
+            // 
+            this.cbxNic.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxNic.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbxNic.FormattingEnabled = true;
+            this.cbxNic.Location = new System.Drawing.Point(74, 6);
+            this.cbxNic.Name = "cbxNic";
+            this.cbxNic.Size = new System.Drawing.Size(538, 20);
+            this.cbxNic.TabIndex = 1;
+            // 
+            // lblNic
+            // 
+            this.lblNic.AutoSize = true;
+            this.lblNic.Location = new System.Drawing.Point(12, 9);
+            this.lblNic.Name = "lblNic";
+            this.lblNic.Size = new System.Drawing.Size(56, 12);
+            this.lblNic.TabIndex = 2;
+            this.lblNic.Text = "Interfaces";
+            // 
+            // lblIPa
+            // 
+            this.lblIPa.AutoSize = true;
+            this.lblIPa.Location = new System.Drawing.Point(12, 95);
+            this.lblIPa.Name = "lblIPa";
+            this.lblIPa.Size = new System.Drawing.Size(51, 12);
+            this.lblIPa.TabIndex = 2;
+            this.lblIPa.Text = "IPアドレス";
+            // 
+            // txtIPaL
+            // 
+            this.txtIPaL.Location = new System.Drawing.Point(92, 92);
+            this.txtIPaL.Name = "txtIPaL";
+            this.txtIPaL.Size = new System.Drawing.Size(119, 19);
+            this.txtIPaL.TabIndex = 3;
+            this.txtIPaL.Text = "172.16.0.1";
+            // 
+            // lblMac
+            // 
+            this.lblMac.AutoSize = true;
+            this.lblMac.Location = new System.Drawing.Point(12, 68);
+            this.lblMac.Name = "lblMac";
+            this.lblMac.Size = new System.Drawing.Size(66, 12);
+            this.lblMac.TabIndex = 2;
+            this.lblMac.Text = "MACアドレス";
+            // 
+            // txtMacL
+            // 
+            this.txtMacL.Location = new System.Drawing.Point(92, 65);
+            this.txtMacL.Name = "txtMacL";
+            this.txtMacL.Size = new System.Drawing.Size(119, 19);
+            this.txtMacL.TabIndex = 3;
+            this.txtMacL.Text = "12-34-56-78-EF-01";
+            // 
+            // lblPayload
+            // 
+            this.lblPayload.AutoSize = true;
+            this.lblPayload.Location = new System.Drawing.Point(16, 196);
+            this.lblPayload.Name = "lblPayload";
+            this.lblPayload.Size = new System.Drawing.Size(79, 12);
+            this.lblPayload.TabIndex = 2;
+            this.lblPayload.Text = "Payload (byte)";
+            // 
+            // lblInv
+            // 
+            this.lblInv.AutoSize = true;
+            this.lblInv.Location = new System.Drawing.Point(16, 169);
+            this.lblInv.Name = "lblInv";
+            this.lblInv.Size = new System.Drawing.Size(85, 12);
+            this.lblInv.TabIndex = 2;
+            this.lblInv.Text = "テスト時間 (sec)";
+            // 
+            // txtInv
+            // 
+            this.txtInv.Location = new System.Drawing.Point(116, 166);
+            this.txtInv.Name = "txtInv";
+            this.txtInv.Size = new System.Drawing.Size(95, 19);
+            this.txtInv.TabIndex = 4;
+            this.txtInv.Text = "10";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(12, 124);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(57, 12);
+            this.lblPort.TabIndex = 2;
+            this.lblPort.Text = "ポート番号";
+            // 
+            // txtPortL
+            // 
+            this.txtPortL.Location = new System.Drawing.Point(92, 121);
+            this.txtPortL.Name = "txtPortL";
+            this.txtPortL.Size = new System.Drawing.Size(119, 19);
+            this.txtPortL.TabIndex = 3;
+            this.txtPortL.Text = "0";
+            // 
+            // lblL
+            // 
+            this.lblL.AutoSize = true;
+            this.lblL.Location = new System.Drawing.Point(90, 39);
+            this.lblL.Name = "lblL";
+            this.lblL.Size = new System.Drawing.Size(67, 12);
+            this.lblL.TabIndex = 2;
+            this.lblL.Text = "ローカル設定";
+            // 
+            // txtIPaR
+            // 
+            this.txtIPaR.Location = new System.Drawing.Point(235, 92);
+            this.txtIPaR.Name = "txtIPaR";
+            this.txtIPaR.Size = new System.Drawing.Size(119, 19);
+            this.txtIPaR.TabIndex = 3;
+            this.txtIPaR.Text = "255.255.255.255";
+            // 
+            // lblR
+            // 
+            this.lblR.AutoSize = true;
+            this.lblR.Location = new System.Drawing.Point(233, 39);
+            this.lblR.Name = "lblR";
+            this.lblR.Size = new System.Drawing.Size(63, 12);
+            this.lblR.TabIndex = 2;
+            this.lblR.Text = "リモート設定";
+            // 
+            // txtMacR
+            // 
+            this.txtMacR.Location = new System.Drawing.Point(235, 65);
+            this.txtMacR.Name = "txtMacR";
+            this.txtMacR.Size = new System.Drawing.Size(119, 19);
+            this.txtMacR.TabIndex = 3;
+            this.txtMacR.Text = "ff-ff-ff-ff-ff-ff";
+            // 
+            // txtPortR
+            // 
+            this.txtPortR.Location = new System.Drawing.Point(235, 121);
+            this.txtPortR.Name = "txtPortR";
+            this.txtPortR.Size = new System.Drawing.Size(119, 19);
+            this.txtPortR.TabIndex = 3;
+            this.txtPortR.Text = "12345";
+            // 
+            // btnRecv
+            // 
+            this.btnRecv.Location = new System.Drawing.Point(376, 190);
+            this.btnRecv.Name = "btnRecv";
+            this.btnRecv.Size = new System.Drawing.Size(115, 23);
+            this.btnRecv.TabIndex = 0;
+            this.btnRecv.Text = "受信テスト開始";
+            this.btnRecv.UseVisualStyleBackColor = true;
+            this.btnRecv.Click += new System.EventHandler(this.btnRecv_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(376, 39);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(236, 142);
+            this.txtResult.TabIndex = 5;
+            // 
+            // txtFrasize
+            // 
+            this.txtFrasize.Location = new System.Drawing.Point(300, 192);
+            this.txtFrasize.Name = "txtFrasize";
+            this.txtFrasize.ReadOnly = true;
+            this.txtFrasize.Size = new System.Drawing.Size(54, 19);
+            this.txtFrasize.TabIndex = 4;
+            // 
+            // lblFrasize
+            // 
+            this.lblFrasize.AutoSize = true;
+            this.lblFrasize.Location = new System.Drawing.Point(230, 196);
+            this.lblFrasize.Name = "lblFrasize";
+            this.lblFrasize.Size = new System.Drawing.Size(64, 12);
+            this.lblFrasize.TabIndex = 2;
+            this.lblFrasize.Text = "= framesize";
+            // 
+            // nudPayload
+            // 
+            this.nudPayload.Location = new System.Drawing.Point(116, 194);
+            this.nudPayload.Maximum = new decimal(new int[] {
+            1472,
+            0,
+            0,
+            0});
+            this.nudPayload.Name = "nudPayload";
+            this.nudPayload.Size = new System.Drawing.Size(95, 19);
+            this.nudPayload.TabIndex = 6;
+            this.nudPayload.Value = new decimal(new int[] {
+            1472,
+            0,
+            0,
+            0});
+            this.nudPayload.ValueChanged += new System.EventHandler(this.nudPayload_ValueChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 224);
+            this.Controls.Add(this.nudPayload);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInv);
+            this.Controls.Add(this.txtFrasize);
+            this.Controls.Add(this.txtPortR);
+            this.Controls.Add(this.txtPortL);
+            this.Controls.Add(this.txtMacR);
+            this.Controls.Add(this.txtMacL);
+            this.Controls.Add(this.lblInv);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblFrasize);
+            this.Controls.Add(this.lblPayload);
+            this.Controls.Add(this.lblR);
+            this.Controls.Add(this.lblL);
+            this.Controls.Add(this.txtIPaR);
+            this.Controls.Add(this.lblMac);
+            this.Controls.Add(this.txtIPaL);
+            this.Controls.Add(this.lblIPa);
+            this.Controls.Add(this.lblNic);
+            this.Controls.Add(this.cbxNic);
+            this.Controls.Add(this.btnRecv);
+            this.Controls.Add(this.btnSend);
+            this.Name = "frmMain";
+            this.Text = "UDP速度計測ツール";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudPayload)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnSend;
+    private System.Windows.Forms.ComboBox cbxNic;
+    private System.Windows.Forms.Label lblNic;
+    private System.Windows.Forms.Label lblIPa;
+    private System.Windows.Forms.TextBox txtIPaL;
+    private System.Windows.Forms.Label lblMac;
+    private System.Windows.Forms.TextBox txtMacL;
+    private System.Windows.Forms.Label lblPayload;
+    private System.Windows.Forms.Label lblInv;
+    private System.Windows.Forms.TextBox txtInv;
+    private System.Windows.Forms.Label lblPort;
+    private System.Windows.Forms.TextBox txtPortL;
+    private System.Windows.Forms.Label lblL;
+    private System.Windows.Forms.TextBox txtIPaR;
+    private System.Windows.Forms.Label lblR;
+    private System.Windows.Forms.TextBox txtMacR;
+    private System.Windows.Forms.TextBox txtPortR;
+    private System.Windows.Forms.Button btnRecv;
+    private System.Windows.Forms.TextBox txtResult;
+    private System.Windows.Forms.TextBox txtFrasize;
+    private System.Windows.Forms.Label lblFrasize;
+    private System.Windows.Forms.NumericUpDown nudPayload;
+  }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Net;
+using SharpPcap;
+using SharpPcap.LibPcap;
+using SharpPcap.WinPcap;
+using System.Runtime.InteropServices;
+
+namespace namapo
+{
+
+    /* IPアドレス用共用体 */
+    [StructLayout(LayoutKind.Explicit, Size = 4)]
+    struct T_IPA
+    {
+        [FieldOffset(0)]
+        public uint dat;
+
+        [FieldOffset(0)]  public byte b0;
+        [FieldOffset(1)]  public byte b1;
+        [FieldOffset(2)]  public byte b2;
+        [FieldOffset(3)]  public byte b3;
+    }
+
+    /* MACアドレス用共用体 */
+    [StructLayout(LayoutKind.Explicit, Size = 6)]
+    struct T_MAC
+    {
+        [FieldOffset(0)]
+        public uint dat1;
+        [FieldOffset(4)]
+        public ushort dat2;
+
+        [FieldOffset(0)]  public byte b0;
+        [FieldOffset(1)]  public byte b1;
+        [FieldOffset(2)]  public byte b2;
+        [FieldOffset(3)]  public byte b3;
+        [FieldOffset(4)]  public byte b4;
+        [FieldOffset(5)]  public byte b5;
+    }
+
+
+  public partial class frmMain : Form
+  {
+      public const short ETH_HDR_SIZE = 14;
+      public const short IP_HDR_SIZE = 20;
+      public const short UDP_HDR_SIZE = 8;
+
+    public frmMain()
+    {
+      InitializeComponent();
+    }
+
+    public void update_ip_csum(byte[] data, int ip_head = 14) 
+    {
+        ulong csum = 0;
+
+        for (int n = ip_head; n <= (ip_head + 18); n += 2)
+        {
+            if (n == (ip_head + 10)) continue;  /* skip csum fields */
+            csum += (ulong)((data[n] << 8) + data[n + 1]);
+        }
+
+        csum = (csum & 0xFFFF) + (csum >> 16);
+        csum = 0xFFFF - csum;
+
+        data[ip_head + 10] = (byte)(csum >> 8);
+        data[ip_head + 11] = (byte)(csum & 0xFF);
+    }
+
+    public byte[] str2hexs(string str, char separator, bool dec = false)
+    {
+        string[] sHex = str.Split(separator);
+        byte[] bHex = new byte[sHex.Length];
+        StringBuilder sb = new StringBuilder(sHex.Length);
+        int n;
+
+        sb.Clear();
+        n = 0;
+        if (dec) {
+            foreach (string sDat in sHex)
+            {
+              bHex[n++] = ("" != sDat) ? Convert.ToByte(sDat, 10) : (byte)0x00;
+            }
+        }
+        else {
+            foreach (string sDat in sHex)
+            {
+              bHex[n++] = ("" != sDat) ? Convert.ToByte(sDat, 16) : (byte)0x00;
+            }
+        }
+
+        return bHex;
+    }
+
+    public void update_udp_csum(byte[] data, int udp_head = 34)
+    {
+        ulong csum = 0;
+        int n;
+        int udp_dlen = (data[udp_head + 4] << 8) + data[udp_head + 5];
+
+        n = udp_head - 8;
+        /* src addr */
+        csum += (ulong)((data[n] << 8) + data[n + 1]);  n += 2;
+        csum += (ulong)((data[n] << 8) + data[n + 1]);  n += 2;
+        /* dst addr */
+        csum += (ulong)((data[n] << 8) + data[n + 1]);  n += 2;
+        csum += (ulong)((data[n] << 8) + data[n + 1]);  n += 2;
+        //csum += 0x0011;
+        csum += (ulong)IPAddress.HostToNetworkOrder((short)0x0011);
+        csum += (ulong)IPAddress.HostToNetworkOrder((short)udp_dlen);   /* UDP data length */
+       
+        
+
+        data[udp_head + 6] = 0;
+        data[udp_head + 7] = 0;
+        for (n = udp_head; n < (udp_head + udp_dlen); n++)
+        {
+            if (0 != ((n - udp_head) & 0x01))
+            {
+                csum += (ulong)(data[n] << 8);
+            }
+            else
+            {
+                csum += data[n];
+            }
+            //csum += (ulong)((data[n] << 8) + data[n + 1]);
+        }
+
+        csum = (csum & 0xFFFF) + (csum >> 16);
+        csum = 0xFFFF - csum;
+
+        data[udp_head + 6] = (byte)(csum & 0xFF);
+        data[udp_head + 7] = (byte)(csum >> 8);
+    }
+
+
+    unsafe private void btnSend_Click(object sender, EventArgs e)
+    {
+        int inv_ms = Convert.ToInt32(txtInv.Text);
+        short framesize = Convert.ToInt16(txtFrasize.Text);
+        byte[] byt;
+
+        byte[] fbyt = new byte[1518];
+        int n = 0;
+
+        /*---- Ethernet ヘッダー ---------------------------------*/
+        /* ローカルMACアドレス */
+        byt = str2hexs(txtMacR.Text, '-');
+        fixed (byte* wk = &fbyt[n])
+        {
+            T_MAC* pmac = (T_MAC*)wk;
+            pmac->b0 = byt[0];
+            pmac->b1 = byt[1];
+            pmac->b2 = byt[2];
+            pmac->b3 = byt[3];
+            pmac->b4 = byt[4];
+            pmac->b5 = byt[5];
+        }
+        n += sizeof(T_MAC);
+
+        /* リモートMACアドレス */
+        byt = str2hexs(txtMacL.Text, '-');
+        fixed (byte* wk = &fbyt[n])
+        {
+            T_MAC* pmac = (T_MAC*)wk;
+            pmac->b0 = byt[0];
+            pmac->b1 = byt[1];
+            pmac->b2 = byt[2];
+            pmac->b3 = byt[3];
+            pmac->b4 = byt[4];
+            pmac->b5 = byt[5];
+        }
+        n += sizeof(T_MAC);
+
+        fbyt[n++] = 0x08;
+        fbyt[n++] = 0x00;
+
+
+        /*---- IP ヘッダー ---------------------------------------*/
+        fbyt[n++] = 0x45;
+        fbyt[n++] = 0x00;
+        fixed (byte* wk = &fbyt[n])     /* Length */
+        {
+            *(short*)wk = IPAddress.HostToNetworkOrder((short)framesize);
+        }
+        n += sizeof(short);
+        fixed (byte* wk = &fbyt[n])     /* Identification */
+        {
+            *(short*)wk = IPAddress.HostToNetworkOrder((short)1);
+        }
+        n += sizeof(short);
+        fbyt[n++] = 0x00;               /* Flags */
+        fbyt[n++] = 0x00;               /* Fragment offset */
+        fbyt[n++] = 0x40;               /* TTL */
+        fbyt[n++] = 0x11;               /* Protocol (UDP) */
+        fbyt[n++] = 0x00;               /* IP checksum */
+        fbyt[n++] = 0x00;
+
+        /* ローカルIPアドレス */
+        byt = str2hexs(txtIPaL.Text, '.', true);
+        fixed (byte* wk = &fbyt[n])
+        {
+            T_IPA* pipa = (T_IPA*)wk;
+            pipa->b0 = byt[0];
+            pipa->b1 = byt[1];
+            pipa->b2 = byt[2];
+            pipa->b3 = byt[3];
+        }
+        n += sizeof(T_IPA);
+
+        /* リモートIPアドレス */
+        byt = str2hexs(txtIPaR.Text, '.', true);
+        fixed (byte* wk = &fbyt[n])
+        {
+            T_IPA* pipa = (T_IPA*)wk;
+            pipa->b0 = byt[0];
+            pipa->b1 = byt[1];
+            pipa->b2 = byt[2];
+            pipa->b3 = byt[3];
+        }
+        n += sizeof(T_IPA);
+
+        /*---- UDP ヘッダー --------------------------------------*/
+        fixed (byte* wk = &fbyt[n])     /* Source Port */
+        {
+            *(short*)wk = IPAddress.HostToNetworkOrder(Convert.ToInt16(txtPortL.Text));
+        }
+        n += sizeof(short);
+
+        fixed (byte* wk = &fbyt[n])     /* Destination Port */
+        {
+            *(short*)wk = IPAddress.HostToNetworkOrder(Convert.ToInt16(txtPortR.Text));
+        }
+        n += sizeof(short);
+
+        fixed (byte* wk = &fbyt[n])     /* Length */
+        {
+            framesize -= IP_HDR_SIZE;
+            *(short*)wk = IPAddress.HostToNetworkOrder((short)framesize);
+        }
+        n += sizeof(short);
+
+        fbyt[n++] = 0x00;               /* UDP checksum */
+        fbyt[n++] = 0x00;
+
+        /* Payload */
+        framesize -= UDP_HDR_SIZE;
+        n += framesize;
+
+        Array.Resize(ref fbyt, n);
+
+
+        /* Update checksum */
+        update_ip_csum(fbyt);
+        update_udp_csum(fbyt);
+
+        /*--------------------------------------------------------*/
+
+        /* SharpPcapデバイスオープン */
+        Debug.WriteLine("SelectedIndex : " + cbxNic.SelectedIndex);
+        LibPcapLiveDevice dev = LibPcapLiveDeviceList.Instance[cbxNic.SelectedIndex];
+        dev.Open();
+
+        for (int x = 0; x < inv_ms; x++)
+        {
+            dev.SendPacket(fbyt);
+            System.Threading.Thread.Sleep(1);
+        }
+        
+        dev.Close();
+    }
+
+    private void frmMain_Load(object sender, EventArgs e)
+    {
+      for (int i = 0; i < LibPcapLiveDeviceList.Instance.Count; i++)
+      {
+        LibPcapLiveDevice dev = LibPcapLiveDeviceList.Instance[i];
+
+        cbxNic.Items.Add(string.Format("{0}: {1} ({2}) - {3}",
+          i, dev.Interface.MacAddress, dev.Interface.Description, dev.Interface.FriendlyName));
+      }
+      cbxNic.SelectedIndex = 0;
+
+      /* デバイス列挙 */
+      foreach (LibPcapLiveDevice dev in LibPcapLiveDeviceList.Instance)
+      {
+        Debug.WriteLine("FriendlyName : " + dev.Interface.FriendlyName);
+        Debug.WriteLine("Name         : " + dev.Interface.Name);
+        Debug.WriteLine("MacAddress   : " + dev.Interface.MacAddress);
+        Debug.WriteLine("Flags        : " + dev.Interface.Flags);
+
+        foreach (PcapAddress addr in dev.Interface.Addresses)
+        {
+          Debug.WriteLine("Addr         :" + addr.Addr);
+          Debug.WriteLine("Netmask      :" + addr.Netmask);
+          Debug.WriteLine("Broadaddr    :" + addr.Broadaddr);
+          Debug.WriteLine("Dstaddr      :" + addr.Dstaddr);
+        }
+
+        Debug.WriteLine("Description  : " + dev.Interface.Description);
+        Debug.WriteLine("");
+      }
+    }
+
+    private void nudPayload_ValueChanged(object sender, EventArgs e)
+    {
+        short framesize;
+
+        framesize = Convert.ToInt16(nudPayload.Value);
+        framesize += ETH_HDR_SIZE;
+        framesize += IP_HDR_SIZE;
+        framesize += UDP_HDR_SIZE;
+
+        txtFrasize.Text = framesize.ToString();
+
+    }
+
+    // イベントハンドラ
+    private static void OnPacketArrival(object sender, CaptureEventArgs e)
+    {
+        DateTime time = e.Packet.Timeval.Date;
+        int len = e.Packet.Data.Length;
+        Debug.WriteLine(String.Format("{0}:{1}:{2},{3} Len={4}",
+                                        time.Hour, time.Minute, time.Second, time.Millisecond, len));
+        Debug.WriteLine(e.Packet.ToString());
+    }
+
+    private void btnRecv_Click(object sender, EventArgs e)
+    {
+        // 参考: http://d.hatena.ne.jp/machi_pon/20100131/1264901795
+
+        /* SharpPcapデバイスオープン */
+        Debug.WriteLine("SelectedIndex : " + cbxNic.SelectedIndex);
+        LibPcapLiveDevice dev = LibPcapLiveDeviceList.Instance[cbxNic.SelectedIndex];
+
+        dev.OnPacketArrival += OnPacketArrival;
+
+        //dev.Filter = "ip.addr==" + txtIPaR.Text;
+        //dev.Filter += " and udp.srcport==" + txtPortR.Text;
+        dev.Open();
+
+        dev.StartCapture();
+        System.Threading.Thread.Sleep(Convert.ToInt32(txtInv.Text) * 1000);
+        dev.StopCapture();
+
+        Debug.WriteLine(dev.Statistics.ToString());
+
+        dev.Close();
+    }
+  }
+}
